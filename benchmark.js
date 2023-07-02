02722392524693c772a03e39de5c74d80e5bed35
Lobarr <jesulobaegunjobi@hotmail.com>
2023-07-02 04:32:59 -0400

    clean up benchmark script

--- benchmark.js
+++ benchmark.js
@@ -1,14 +1,12 @@
 import http from 'k6/http';
-import { sleep } from 'k6';
+import { check } from 'k6';
 
 export const options = {
   stages: [
-    { duration: '30s', target: 100 },
-    { duration: '30s', target: 500 },
-    { duration: '30s', target: 1000 },
-    { duration: '30s', target: 5000 },
-    { duration: '30s', target: 10000 },
-    { duration: '30s', target: 25000 },
+    { duration: '20s', target: 10 },
+    { duration: '20s', target: 100 },
+    { duration: '20s', target: 500 },
+    { duration: '20s', target: 1000 },
   ],
 };
 
@@ -28,7 +26,7 @@ const keys = Array.from({ length: 10 }, () => generateRandomString(100));
 
 export default function () {
   for (const key of keys) {
-    const value = generateRandomString(1000000);
+    const value = generateRandomString(10000);
 
     const set_response = http.post(`http://localhost:9998/keys/${key}`, value);
     check(set_response, { 'set request suceeded': (r) => r.status == 200 });
